9f946e5f3a92b58921751bf3aac0bbf43b5523ce
thcheetah777 <thcheetah777@gmail.com>
2023-08-05 18:24:09 +0300

    Basic search functionality

--- src/routes/(main)/posts/+page.server.ts
+++ src/routes/(main)/posts/+page.server.ts
@@ -1,16 +1,34 @@
 import type { PageServerLoad } from './$types';
-import { queryDocs, type PostData, fetchDocs } from '$lib/firebase';
+import { type PostData, fetchDocs } from '$lib/firebase';
 
 export const load = (async ({ url }) => {
-  const q = url.searchParams.get("q");
-  const tags = url.searchParams.get("tags");
+  // const q = url.searchParams.get("q");
+  // const type = url.searchParams.get("type");
+  // const tags = url.searchParams.get("tags");
 
-  console.log("Query: ", q ?? "none");
-  console.log("Tags: ", tags?.split(",") ?? []);
+  // const query = { q, type, tags };
 
-  const posts = await fetchDocs<PostData>("posts");
+  // console.log("Query: ", q ?? "");
+  // console.log("Typpe: ", type ?? "");
+  // console.log("Tags: ", tags?.split(",") ?? []);
 
-  return {
-    posts
-  };
+  // let posts = await fetchDocs("posts") as PostData[];
+  // posts = posts.filter(post => {
+  //   const titleHasQ = q ? post.title.toLowerCase().includes(q.toLowerCase()) : true;
+  //   const qHasTitle = q ? q.toLowerCase().includes(post.title.toLowerCase()) : true;
+  //   const qType = type ? post.type === type : true;
+  //   const tagsHasQ = tags ? post.tags.some(tag => tags.split(",").includes(tag)) : true;
+
+  //   console.log(post.title, titleHasQ, qHasTitle, qType, tagsHasQ);
+
+  //   return (
+  //     (titleHasQ || qHasTitle) && qType && tagsHasQ
+  //   );
+  // });
+
+  // return {
+  //   query,
+  //   posts
+  // };
+  return {};
 }) satisfies PageServerLoad;
--- src/routes/(main)/posts/+page.svelte
+++ src/routes/(main)/posts/+page.svelte
@@ -1,12 +1,47 @@
 <script lang="ts">
   import type { PageData } from './$types';
+  import { onMount } from 'svelte';
+  import { fetchDocs, type PostData, type PostType } from '$lib/firebase';
   import PostCard from '$lib/components/PostCard.svelte';
+  import { page } from '$app/stores';
+  import Spinner from '$lib/components/Spinner.svelte';
 
   export let data: PageData;
+
+  const q: string = $page.url.searchParams.get("q") ?? "";
+  const type: PostType | null = $page.url.searchParams.get("type") as PostType;
+  const tags: string[] = $page.url.searchParams.get("tags")?.split(",") ?? [];
+
+  let loading = true;
+  let posts: PostData[] = [];
+
+  onMount(async () => {
+    loading = true;
+
+    posts = await fetchDocs("posts") as PostData[];
+    posts = posts.filter(post => {
+      const titleHasQ = q ? post.title.toLowerCase().includes(q.toLowerCase()) : true;
+      const qHasTitle = q ? q.toLowerCase().includes(post.title.toLowerCase()) : true;
+      const qType = type ? post.type === type : true;
+      const tagsHasQ = tags.length ? post.tags.some(tag => tags.includes(tag)) : true;
+
+      console.log(post.title, titleHasQ, qHasTitle, qType, tagsHasQ);
+
+      return (
+        (titleHasQ || qHasTitle) && qType && tagsHasQ
+      );
+    });
+
+    loading = false;
+  });
 </script>
 
+{#if loading}
+  <Spinner />
+{/if}
+
 <div class="grid grid-cols-3 gap-4 m-4">
-  {#each data.posts as post}
+  {#each posts as post}
     <PostCard post={post} urlPrefix="/post" width="100%" />
   {/each}
 </div>
