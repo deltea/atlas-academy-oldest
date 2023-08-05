b4a23de748d0d93afcf3365c434889cdcdca6147
thcheetah777 <thcheetah777@gmail.com>
2023-08-05 10:15:54 +0300

    Get search query

--- src/routes/(main)/posts/+page.server.ts
+++ src/routes/(main)/posts/+page.server.ts
@@ -1,7 +1,13 @@
 import type { PageServerLoad } from './$types';
-import { fetchDocs, type PostData } from '$lib/firebase';
+import { queryDocs, type PostData, fetchDocs } from '$lib/firebase';
 
-export const load = (async () => {
+export const load = (async ({ url }) => {
+  const q = url.searchParams.get("q");
+  const tags = url.searchParams.get("tags");
+
+  console.log("Query: ", q ?? "none");
+  console.log("Tags: ", tags?.split(",") ?? []);
+
   const posts = await fetchDocs<PostData>("posts");
 
   return {
