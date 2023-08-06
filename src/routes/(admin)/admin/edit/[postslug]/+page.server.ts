3f51d5f9965a9f567345328f59d22740f6452ace
thcheetah777 <thcheetah777@gmail.com>
2023-08-06 18:47:29 +0300

    Fix security vlunerability for admin pages

--- src/routes/(admin)/admin/edit/[postslug]/+page.server.ts
+++ src/routes/(admin)/admin/edit/[postslug]/+page.server.ts
@@ -1,7 +1,9 @@
 import { queryDocs, type PostData } from '$lib/firebase';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ params }) => {
+export const load = (async ({ params, parent }) => {
+  await parent();
+
   const results = await queryDocs("posts", "slug", "==", params.postslug, 1);
   const post = results[0] as PostData;
 
--- src/routes/(admin)/admin/map/+page.server.ts
+++ src/routes/(admin)/admin/map/+page.server.ts
@@ -1,7 +1,9 @@
 import { fetchDocs, type MarkerData } from '$lib/firebase';
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
+export const load = (async ({ parent }) => {
+  await parent();
+
   const markers = await fetchDocs<MarkerData>("markers") as MarkerData[];
 
   return {
