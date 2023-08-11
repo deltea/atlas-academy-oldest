24b6cfae90ad64a67b67636f10f419fda889dfe9
thcheetah777 <thcheetah777@gmail.com>
2023-08-11 20:28:13 +0300

    Fix bug with marker overlapping

--- src/lib/components/Marker.svelte
+++ src/lib/components/Marker.svelte
@@ -12,13 +12,13 @@
 
 <a
   href="/destinations/{tag}"
-  class="absolute z-10 inline-block group"
+  class="absolute inline-block group"
   style:left="calc({x}% - 0.75rem)"
   style:top="calc({y}% - 0.75rem)"
   transition:scale={{ duration: 200 }}
   data-sveltekit-reload
 >
-  <IconMarker class="text-red-500 text-xl group-hover:scale-150 duration-300 origin-bottom" />
+  <IconMarker class="text-red-500 text-xl group-hover:scale-150 duration-300 origin-bottom z-10" />
   <div class="tooltip-content w-40 bg-neutral rounded-lg p-2 z-20 -top-32 opacity-0 invisible group-hover:opacity-100 group-hover:visible duration-300">
     <h1 class="text-md font-bold mb-2">{name}</h1>
     <p class="text-sm">{description}</p>
