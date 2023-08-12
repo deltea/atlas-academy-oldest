26c7b8595bab7e9e29ba945ee5a6fb1c821a38b6
thcheetah777 <thcheetah777@gmail.com>
2023-08-12 07:59:02 +0300

    Add content to homepage

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -6,48 +6,26 @@
 </script>
 
 <PageLanding center src="/images/landing.jpg">
-  <h2 class="tracking-widest -mt-28">oga booga lau iowjioje</h2>
-  <h1 class="text-3xl tracking-wider">BLAH BLAH BLAH</h1>
+  <h2 class="tracking-widest -mt-28">啟程向世界學習</h2>
+  <h1 class="text-4xl tracking-wider font-title">世界是学校</h1>
 </PageLanding>
 
 <main class="p-sm">
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nulla iure illo! Quis, quos nihil! Dolore necessitatibus voluptatem, quas eaque fuga ullam porro accusamus qui, tempore fugiat accusantium labore eum!
-  Dolorem neque, ratione fuga hic libero ad nesciunt, nam repellat minus illum, magni harum iusto ab cum at voluptas! Numquam ab ipsa nemo architecto magni rem laborum soluta sunt fugit.
-  Veniam molestias aspernatur earum cupiditate, et eligendi officiis! Assumenda modi eius totam, fugiat quisquam aliquid numquam, eveniet pariatur obcaecati dolorum temporibus voluptatibus, a ullam cupiditate amet. Amet ad laudantium deleniti!
-  Ipsum culpa enim necessitatibus itaque consequuntur voluptate vero eum ipsa quam eos. Architecto possimus dolore dolor! Quo sequi ut aspernatur blanditiis similique vel modi voluptates animi ratione, adipisci, minima molestiae.
-  Doloribus voluptatem accusantium magnam ducimus hic error! Sequi ipsum quo dicta sed laboriosam tempora quidem ratione nisi accusamus, enim voluptate voluptatem amet similique neque inventore voluptatum. Autem ea corrupti doloribus!
-  Quasi in nulla commodi officiis fuga quo optio nemo iure laudantium eaque libero modi ipsa, neque itaque a dolore, sapiente vel cum? Fugiat soluta quam perferendis dicta possimus blanditiis explicabo!
-  Culpa qui optio quibusdam. Tempore maxime corrupti molestias fuga, dolores harum ut et corporis aliquid numquam accusantium, officia ducimus mollitia cum optio modi, eveniet deleniti dolore accusamus quae sequi velit.
-  Repellendus eos ad quae nihil illum reiciendis expedita natus! Totam soluta architecto recusandae voluptas earum, dolorem sequi necessitatibus, expedita ad nostrum deserunt deleniti maiores, quidem placeat vitae magni dicta nisi.
-  Minima nihil laboriosam voluptas sapiente assumenda natus veniam esse fugiat sequi, perferendis accusamus delectus sint eveniet placeat harum iure dolor aperiam in perspiciatis repellendus soluta, itaque quia? Nesciunt, reprehenderit libero?
-  Cupiditate veniam laboriosam expedita excepturi eveniet officia accusamus ut officiis animi, esse, quas ad repellat sit nesciunt quae, tempora earum suscipit doloribus. Quisquam aperiam itaque tenetur consequuntur provident quia maiores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque repellendus nam, tenetur, quibusdam unde fugit aliquam ex commodi cum blanditiis saepe, aperiam atque optio ut! Dolorem distinctio quidem fuga! Obcaecati!
-  Saepe esse sed ut molestias debitis! Architecto, nobis! Eum qui, atque in quo omnis itaque suscipit similique iste quos corporis quia quam. Quo iste incidunt veritatis eaque eius omnis dolorum.
-  Odit laboriosam alias vel velit esse dolorem inventore omnis, impedit quos excepturi eius magni quod, perferendis eaque accusantium necessitatibus earum. Nostrum voluptatum unde aliquam labore laborum laboriosam nemo asperiores praesentium?
-  Harum dolor maiores excepturi ut quas quasi nam, aspernatur ducimus. Ea fuga vitae est ipsa officiis modi, perferendis sint perspiciatis quisquam, repudiandae ut nemo natus corrupti ipsum reprehenderit unde magni?
-  Obcaecati culpa sunt cupiditate fugiat incidunt sit. Qui nihil accusantium beatae officia est nesciunt totam magnam earum nam temporibus voluptate, excepturi, recusandae maiores assumenda quibusdam possimus quam iste quidem animi?
-  Alias nisi reprehenderit quos sed asperiores recusandae. Error, ab molestias officiis velit quos deleniti natus sequi, inventore, vero aliquid labore nulla aut sed quis incidunt quam sunt at dolore dignissimos!
-  Natus voluptatibus voluptatem rem sequi quidem sapiente ipsam ea eum, voluptas tenetur ut, deleniti hic aut consequatur ratione velit cupiditate reiciendis, eligendi a eaque! Molestiae unde consequuntur eaque officiis commodi.
-  Vel aliquam doloremque vero nobis nulla dignissimos numquam adipisci eum consectetur obcaecati. Maiores consectetur, quibusdam voluptate corporis cupiditate earum libero beatae, accusantium numquam debitis velit perferendis facere! Quia, laudantium aperiam.
-  Aliquam placeat id tenetur esse architecto. Quas quae animi recusandae voluptate autem ea minus explicabo? Saepe sed eos, perferendis quos neque qui impedit, minus sapiente ratione beatae modi rem nemo.
-  Quasi omnis ratione quod maxime dolorem. Illo, velit nemo. Dolorum illum aliquid dignissimos maxime totam voluptate rerum. Voluptas maiores quidem possimus ducimus, perspiciatis expedita saepe veritatis a, inventore magnam odio.
-  Neque minima fuga soluta esse eligendi dolorem quod voluptatum natus voluptas itaque accusantium, quia repellendus velit expedita ex officia magni odio architecto omnis culpa molestiae, qui aut est sint. Minus.
-  Soluta eos odit, id nemo cupiditate sint tempora officiis veritatis dolorem consectetur dolores tenetur quidem explicabo magnam ut esse! Ut quaerat ipsa neque maxime architecto ducimus id libero quasi at?
-  At accusamus adipisci voluptates quas. Quam, aliquam voluptatibus reprehenderit labore eveniet nostrum nisi reiciendis doloribus earum, cupiditate velit. Molestias dolores ex repudiandae, officia deleniti rem veritatis placeat repellendus ipsam vitae?
-  Dolores, illum dolor ipsum, quidem necessitatibus possimus quaerat cumque quae perferendis distinctio maxime tempore minus beatae modi? Et accusantium laboriosam excepturi doloribus error. Pariatur, architecto doloremque dolor nemo vel voluptas.
-  A ullam, sint veniam aliquam maxime enim corporis, nulla excepturi sit voluptates adipisci? Saepe nemo reiciendis ullam, vitae sequi delectus illum hic amet placeat dolores provident voluptatem, laboriosam numquam autem!
-  Quibusdam fugit velit quidem exercitationem ratione assumenda earum eveniet praesentium, eum harum ipsam voluptate reiciendis ea tempora atque dolorem id omnis sunt ab totam architecto dolore commodi! Ipsam, distinctio necessitatibus?
-  Ab nam doloremque perspiciatis doloribus magnam sit, corporis maxime quisquam, debitis at sed, omnis voluptates officiis temporibus praesentium tempora harum inventore libero tenetur explicabo asperiores aspernatur ex? Obcaecati, aliquam temporibus?
-  Sit tempore quis dicta ducimus debitis eius saepe iusto ea, soluta, reiciendis, culpa placeat doloremque. Animi sequi obcaecati fuga ad fugit veritatis et, consectetur, sit, labore doloremque voluptas laborum rerum!
-  Cum, eveniet? Ex ipsa aliquam non mollitia dolore commodi magnam consectetur, iste ducimus repudiandae. Beatae nulla, aperiam, fugiat voluptatum laborum eos optio consequuntur nemo, saepe omnis nostrum. Impedit, unde laborum!
-  Aliquam blanditiis ipsam esse tenetur assumenda dolor et libero repudiandae nesciunt recusandae. Natus velit recusandae, culpa cumque atque autem dolorum temporibus delectus iusto accusamus itaque porro praesentium obcaecati maxime quis?
-  Nemo, dolor! Animi, soluta? Laboriosam ratione consectetur, totam modi aspernatur velit veniam eos debitis unde enim nam nobis omnis iure eligendi sapiente quod. In nemo fuga distinctio eum vel. Ipsam!
-  Distinctio odit velit id, esse maiores est beatae. Delectus fugiat nulla possimus quos vel accusamus rerum mollitia ipsa non! Ea magnam quod odio ipsa suscipit itaque optio qui accusantium fugiat.
-  Officia illum cupiditate modi similique iste amet unde, dolorum id eligendi nam aperiam ullam voluptatibus minus esse hic beatae quaerat veritatis illo qui provident maiores, a, recusandae veniam possimus! Ducimus.
-  Id officiis dolore sequi accusantium iste! Voluptates, laborum ad? Incidunt repellat hic fugiat facilis cum provident nam optio libero praesentium! Deleniti voluptate, id quidem officiis consectetur architecto omnis saepe aliquid?
-  Blanditiis dicta et similique architecto soluta mollitia quam, perspiciatis explicabo quas sunt repellendus earum quaerat molestias quia! Repudiandae voluptate, assumenda aspernatur necessitatibus ut iusto voluptatum nostrum eaque fugiat! Voluptatem, eligendi?
-  Error, recusandae unde voluptas libero, nobis natus dicta possimus velit ad explicabo adipisci, corrupti alias id commodi ut asperiores earum dolorem porro itaque eos necessitatibus. Ipsum, dolores! Obcaecati, recusandae cum!
-  Nesciunt, ea repellendus repellat voluptatem perferendis vel magni nisi tenetur? Magni voluptatum perferendis odio quasi. Itaque repudiandae esse blanditiis qui unde aperiam accusantium perferendis ratione voluptates! Quis explicabo cupiditate praesentium?
-  Enim quis vel molestias atque culpa, repudiandae eum nam laudantium nihil deleniti? Adipisci quos accusamus velit? Dolore, voluptas! Quidem magnam earum voluptatibus, iure iusto excepturi veniam laudantium distinctio quae suscipit.
-  Eligendi ex molestiae culpa laborum quam? Expedita enim facere sequi culpa recusandae. Fugit expedita molestiae distinctio, quidem optio eligendi vel tenetur nostrum reiciendis magni maiores voluptatem, suscipit doloribus possimus perferendis?
-  Nihil aliquid accusantium voluptate molestiae ea earum suscipit repellat velit reiciendis laborum voluptatem cum sequi vitae, odit, sit optio porro illo quas sint voluptates! Amet atque eveniet maiores sit adipisci?</p>
+  <section class="flex items-stretch gap-8">
+    <div class="flex flex-col w-full gap-4">
+      <div class="bg-[url(/images/home1.jpg)] rounded-md w-full h-1/2 bg-cover bg-center"></div>
+      <div class="bg-[url(/images/home2.jpg)] rounded-md w-full h-1/2 bg-cover bg-center"></div>
+    </div>
+
+    <div>
+      <h1 class="font-bold text-2xl text-white mb-12">如果有一天，有人問你，你可以居住在世界上任何一個地方，你想住哪裡，你會怎麼回答？</h1>
+
+      <div class="grid gap-4">
+        <p>在疫情期間，我們一家生活和工作都移至線上，對生活的方式和態度也有很大的改變，想要跳脫本來一成不變的生活，打破我們遵循了好多年的社會規範，把視野放向世界，做個地球公民，於是我們有了一個大膽的想法 -</p>
+        <p>環。遊。世。界。</p>
+        <p>把一個家，連根拔起，收進一個行李裡，帶著兩個孩子，花兩年時間到世界各地過著遊牧生活，在每一個城市花時間築巢， 在當地的市場買東西，適應不同的氣候，接觸不同的文化和語言，這聽起來浪漫無比的想法，對我們來說，是一場冒險和挑戰，相信會換來我們一輩子難忘的回憶及經歷。</p>
+        <p>世界是學校，跟我們一起 World Schooling!</p>
+      </div>
+    </div>
+  </section>
 </main>
