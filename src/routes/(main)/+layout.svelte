e624945fe98c29f52efd89a7d8c86ac75d1dfd1f
Leo <thcheetah777@gmail.com>
2023-08-14 12:10:53 +0300

    Make go to top button work in both modes

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -2,26 +2,44 @@
   import type { LayoutData } from "./$types";
   import NavBar from "$lib/components/NavBar.svelte";
   import Footer from "$lib/components/Footer.svelte";
+    import { onMount } from "svelte";
+    import { fly } from "svelte/transition";
 
   import IconUp from "~icons/gg/chevron-up";
 
   export let data: LayoutData;
 
+  let atTopOfPage = true;
+
   function goToTop() {
     window.scrollTo(0, 0);
   }
+
+  function checkTopOfPage() {
+    atTopOfPage = window.scrollY === 0;
+  }
+
+  onMount(() => {
+    checkTopOfPage();
+
+    document.addEventListener("scroll", checkTopOfPage);
+    () => document.removeEventListener("scroll", checkTopOfPage);
+  });
 </script>
 
 <NavBar isAdmin={data.isAdmin} />
 
 <div class="bg-white dark:bg-normal text-normal dark:text-white duration-300">
   <slot />
-  <button
-    class="fixed bottom-8 right-8 dark:bg-neutral-700 bg-neutral-500 text-white rounded-full p-4 shadow-lg"
-    on:click={goToTop}
-  >
-    <IconUp />
-  </button>
+  {#if !atTopOfPage}
+    <button
+      class="fixed bottom-8 right-8 dark:bg-neutral-700 bg-neutral-500 text-white rounded-full p-4 shadow-lg"
+      on:click={goToTop}
+      transition:fly={{ y: 84, duration: 300, opacity: 1 }}
+    >
+      <IconUp />
+    </button>
+  {/if}
 </div>
 
 <Footer />
