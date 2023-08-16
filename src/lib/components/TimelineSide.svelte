c1c2ba453b48ef04d6bb06a5829662eb9c901b8e
Leo <thcheetah777@gmail.com>
2023-08-16 09:12:52 +0300

    Make timeline responsive (kinda)

--- src/lib/components/TimelineSide.svelte
+++ src/lib/components/TimelineSide.svelte
@@ -11,10 +11,12 @@
     <a href="/post/{post.slug}" class="flex flex-col {side === "left" ? "items-end" : "items-start"} group gap-2 relative px-16">
       <div class:text-right={side === "left"}>
         <h1 class="font-semibold dark:group-hover:text-white group-hover:text-normal text-neutral-400 duration-300">{formatDate(post.date, "sm")}</h1>
-        <h2 class="dark:group-hover:text-white group-hover:text-normal text-neutral-400 duration-300 -mt-0.5">{post.title}</h2>
+        <h2 class="dark:group-hover:text-white group-hover:text-normal text-neutral-400 duration-300 -mt-0.5 lg:block hidden">
+          {post.title}
+        </h2>
       </div>
 
-      <img src="{getImage(post.cover, "sm")}" alt="{post.title}" class="h-20 group-hover:brightness-100 brightness-75 group-hover:scale-105 duration-200 rounded-lg">
+      <img src="{getImage(post.cover, "sm")}" alt="{post.title}" class="h-20 w-auto group-hover:brightness-100 brightness-75 group-hover:scale-105 duration-200 rounded-lg">
 
       <div class="absolute w-12 border dark:border-neutral-500 border-neutral-300 dark:group-hover:border-white group-hover:border-normal duration-300 {side === "left" ? "right-0" : "left-0"} top-3"></div>
 
